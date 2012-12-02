1ccfe00fe8feaa6c85e7d36b8dd269b31207ee3c
Andrew Dodd <atd7@cornell.edu>
2012-12-02 01:06:17 -0500

    mali: Pull from insignal repos
    Change-Id: Ib28caa2ced11ce2d9f1ba1cf27be7a38da4cd690

--- drivers/media/video/samsung/mali/common/pmm/mali_pmm.c
+++ drivers/media/video/samsung/mali/common/pmm/mali_pmm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 ARM Limited. All rights reserved.
+ * Copyright (C) 2010-2012 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -25,7 +25,6 @@
 #include "mali_pmm_policy.h"
 #include "mali_pmm_pmu.h"
 #include "mali_platform.h"
-#include "mali_kernel_pm.h"
 
 /* Internal PMM subsystem state */
 static _mali_pmm_internal_state_t *pmm_state = NULL;
@@ -638,18 +637,17 @@ _mali_osk_errcode_t malipmm_powerup( u32 cores )
         _mali_pmm_internal_state_t *pmm = GET_PMM_STATE_PTR;
 
 	/* If all the cores are powered down, power up the MALI */
-        if (pmm->cores_powered == 0) {
-		mali_platform_power_mode_change(MALI_POWER_MODE_ON);
+        if (pmm->cores_powered == 0)
+        {
+                mali_platform_power_mode_change(MALI_POWER_MODE_ON);
 #if MALI_PMM_RUNTIME_JOB_CONTROL_ON
 		/* Initiate the power up */
-		if (_mali_osk_pmm_dev_activate() < 0) {
-			MALI_PRINT(("PMM: Try again PD_G3D enable\n"));
-			if (mali_pd_enable() < 0) {
-				MALI_PRINT(("PMM: Mali PMM device activate failed\n"));
-				err = _MALI_OSK_ERR_FAULT;
-				return err;
-			}
-		}
+                if (_mali_osk_pmm_dev_activate() < 0) {
+                                       MALI_PRINT(("PMM: Mali PMM device activate failed\n"));
+                                       err = _MALI_OSK_ERR_FAULT;
+                                       return err;
+                               }
+
 #endif
         }
 
