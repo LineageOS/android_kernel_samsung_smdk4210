8b0ca722e313874f9916b39c8a188d9624d1cab4
codeworkx <codeworkx@cyanogenmod.org>
2013-01-29 16:37:31 +0000

    mali: downgrade to r2p4
    Change-Id: Id76bd681921fd09e62b611230fa8f7de0bbfdbd7

--- drivers/media/video/samsung/mali/linux/mali_osk_notification.c
+++ drivers/media/video/samsung/mali/linux/mali_osk_notification.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2012 ARM Limited. All rights reserved.
+ * Copyright (C) 2010 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -15,6 +15,8 @@
 
 #include "mali_osk.h"
 #include "mali_kernel_common.h"
+#include "mali_pmm.h"
+#include "mali_pmm_state.h"
 
 /* needed to detect kernel version specific code */
 #include <linux/version.h>
@@ -65,8 +67,12 @@ _mali_osk_notification_t *_mali_osk_notification_create( u32 type, u32 size )
 	/* OPT Recycling of notification objects */
     _mali_osk_notification_wrapper_t *notification;
 
-	notification = (_mali_osk_notification_wrapper_t *)kmalloc( sizeof(_mali_osk_notification_wrapper_t) + size,
-	                                                            GFP_KERNEL | __GFP_HIGH | __GFP_REPEAT);
+	if (MALI_PMM_NOTIFICATION_TYPE == type) {
+		if (size != sizeof(mali_pmm_message_t))
+			return NULL;
+	}
+
+	notification = (_mali_osk_notification_wrapper_t *)kmalloc( sizeof(_mali_osk_notification_wrapper_t) + size, GFP_KERNEL );
     if (NULL == notification)
     {
 		MALI_DEBUG_PRINT(1, ("Failed to create a notification object\n"));
@@ -86,6 +92,7 @@ _mali_osk_notification_t *_mali_osk_notification_create( u32 type, u32 size )
 	}
 
 	/* set up the non-allocating fields */
+	notification->data.magic_code = 0x31415926;
 	notification->data.notification_type = type;
 	notification->data.result_buffer_size = size;
 
@@ -100,6 +107,8 @@ void _mali_osk_notification_delete( _mali_osk_notification_t *object )
 
     notification = container_of( object, _mali_osk_notification_wrapper_t, data );
 
+	/* Remove from the list */
+	list_del(&notification->list);
 	/* Free the container */
 	kfree(notification);
 }
@@ -160,7 +169,15 @@ _mali_osk_errcode_t _mali_osk_notification_queue_dequeue( _mali_osk_notification
 		wrapper_object = list_entry(queue->head.next, _mali_osk_notification_wrapper_t, list);
 		*result = &(wrapper_object->data);
 		list_del_init(&wrapper_object->list);
-		ret = _MALI_OSK_ERR_OK;
+
+		if (wrapper_object->data.magic_code != 0x31415926) {
+			MALI_PRINT(("SEC WARNING : list entry magic_code not match : %x\n", wrapper_object->data.magic_code));
+			MALI_PRINT(("SEC WARNING : list entry notification type : %x\n", wrapper_object->data.notification_type));
+			MALI_PRINT(("SEC WARNING : list entry result buffer size : %x\n", wrapper_object->data.result_buffer_size));
+			MALI_PRINT(("SEC WARNING : list entry result buffer : %x\n", wrapper_object->data.result_buffer));
+		} else {
+			ret = _MALI_OSK_ERR_OK;
+		}
 	}
 
 	up(&queue->mutex);
